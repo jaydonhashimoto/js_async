f5b5ce7ad570230a5376c8250eb559da98a25046
jaydonhashimoto <jaydonhashi@gmail.com>
2019-06-26 16:16:28 -0700

    uncommented code

--- promises.js
+++ promises.js
@@ -52,9 +52,9 @@ function createPost(post) {
 }
 
 /**********************************Create and Get Posts************************************/
-// createPost({ title: 'Post Three', body: 'This is post three' })
-//     .then(getPosts)
-//     .catch(err => console.log(err));
+createPost({ title: 'Post Three', body: 'This is post three' })
+    .then(getPosts)
+    .catch(err => console.log(err));
 
 
 
@@ -77,11 +77,11 @@ function createPost(post) {
  *
  * wait for create post before calling getPosts
  */
-// async function init() {
-//     await createPost({ title: 'Post Three', body: 'This is post three' });
-//     getPosts();
-// }
-// init();
+async function init() {
+    await createPost({ title: 'Post Three', body: 'This is post three' });
+    getPosts();
+}
+init();
 
 
 
@@ -91,12 +91,12 @@ function createPost(post) {
  * Async / Await with fetch
  * get users, convert to json, log
  */
-// async function fetchUsers() {
-//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-//     const data = await res.json();
-//     console.log(data);
-// }
-// fetchUsers();
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+}
+fetchUsers();
 
 
 
@@ -108,11 +108,11 @@ function createPost(post) {
  * It rejects with the reason of the first promise that rejects.
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
  */
-// const promise1 = Promise.resolve('Hello World');
-// const promise2 = 10;
-// const promise3 = new Promise((resolve, reject) =>
-//     setTimeout(resolve, 2000, 'Goodbye'));
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-// Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
+Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
 
